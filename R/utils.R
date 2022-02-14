4786191b6027e578a3607194c1c624c128090d05
kim0sun <kim0sun@korea.ac.kr>
2022-02-14 11:25:31 +0900

    manuscript - code chunk option

--- R/utils.R
+++ R/utils.R
@@ -19,7 +19,15 @@ pdf_cv <- function(...) {
 pdf_manuscript <- function(...) {
    tmplt <- system.file("rmarkdown", "templates", "manuscript", "resources", "template.tex",
                         package = "ysktmplt")
-   bookdown::pdf_document2(template = tmplt, toc = FALSE, ...)
+   out <- bookdown::pdf_document2(template = tmplt, toc = FALSE, ...)
+
+   out$knitr$opts_chunk$prompt = TRUE
+   # out$knitr$opts_chunk$comment = NA
+   out$knitr$opts_chunk$highlight = FALSE
+   out$knitr$opts_chunk$tidy = FALSE
+   out$knitr$opts_chunk$R.options = list(prompt = "R> ", continue = "+ ")
+
+   out
 }
 
 #' @export
